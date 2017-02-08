92810f8786bcb47f4538f1d910a1b04cc656ad34
Romain <Romain@DESKTOP-1J0HHS5>
2017-02-08 19:48:10 +0100

    Ajoutez des fichiers projet.

---
+++ LemonWayChallenge.Client/Program.cs
@@ -0,0 +1,73 @@
+﻿using LemonWayChallenge.Client.LemonWayChallengeServiceReference;
+using System;
+using System.ServiceModel;
+
+namespace LemonWayChallenge.Client
+{
+    static class Program
+    {
+        private const int DEFAULT_NUMBER = 10;
+
+        static void Main(string[] args)
+        {
+            int number;
+
+            if (!((args.Length == 1) && int.TryParse(args[0], out number)))
+            {
+                number = DEFAULT_NUMBER;
+            }
+
+            ServiceSoapClient service = new ServiceSoapClient();
+
+            try
+            {
+                Console.WriteLine($"Calculating {number.Ordinal()} Fibonacci sequence number: {service.Fibonacci(number)}");
+            }
+            catch (EndpointNotFoundException exception)
+            {
+                Console.WriteLine(exception.InnerException == null ? exception.Message : exception.InnerException.Message);
+                Exit(true);
+            }
+            catch (FaultException exception)
+            {
+                Console.WriteLine(exception.InnerException == null ? exception.Message : exception.InnerException.Message);
+                Exit(true);
+            }
+
+            Exit();
+        }
+
+        private static void Exit(bool exitWithError = false)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            if (exitWithError)
+            {
+                Environment.Exit(1);
+            }
+
+            Environment.Exit(0);
+        }
+
+        public static string Ordinal(this int number)
+        {
+            var work = number.ToString();
+            if ((number % 100) == 11 || (number % 100) == 12 || (number % 100) == 13)
+            {
+                return work + "th";
+            }
+
+            switch (number % 10)
+            {
+                case 1: work += "st"; break;
+                case 2: work += "nd"; break;
+                case 3: work += "rd"; break;
+                default: work += "th"; break;
+            }
+
+            return work;
+        }
+    }
+}
---
+++ LemonWayChallenge.WebService/Service.asmx.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Web.Services;
+using System.Xml;
+
+namespace LemonWayChallenge.WebService
+{
+    [WebService(Namespace = "http://romainduclos.com/webservices/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public int Fibonacci(int n)
+        {
+            throw new System.Exception("This is bad");
+
+            if ((n < 1) || (n > 100))
+            {
+                return -1;
+            }
+
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) +  Fibonacci(n - 2);
+        }
+
+        [WebMethod]
+        public string XmlToJson(string xml)
+        {
+            XmlDocument document = new XmlDocument();
+
+            try
+            {
+                document.LoadXml(xml);
+            }
+            catch (XmlException)
+            {
+                return "Bad Xml format";
+            }
+
+            return JsonConvert.SerializeXmlNode(document);
+        }
+    }
+}
