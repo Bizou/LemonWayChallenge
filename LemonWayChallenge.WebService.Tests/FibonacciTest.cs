f867953494db65ab19c5cc0ece4420cd2e334fa4
Romain <romain.duclos@gmail.com>
2017-02-09 17:14:43 +0100

    Ajout d'un projet de test.

---
+++ LemonWayChallenge.WebService.Tests/FibonacciTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LemonWayChallenge.WebService.Tests
+{
+    [TestClass]
+    public class FibonacciTest
+    {
+        private Service m_service;
+        private TestContext m_testContext;
+
+        public Service ServiceContext
+        {
+            get { return m_service; }
+            set { m_service = value; }
+        }
+
+        public TestContext TestContext
+        {
+            get { return m_testContext; }
+            set { m_testContext = value; }
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ServiceContext = new Service();
+        }
+
+        [TestMethod]
+        [DeploymentItem("FibonacciTestData.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                           "|DataDirectory|\\FibonacciTestData.xml",
+                           "Row",
+                            DataAccessMethod.Random)]
+        public void FibonacciWorks()
+        {
+            int n = Convert.ToInt32(TestContext.DataRow["N"]);
+            int expected = Convert.ToInt32(TestContext.DataRow["Result"]);
+
+            int actual = ServiceContext.Fibonacci(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ServiceContext.Dispose();
+        }
+    }
+}
---
+++ LemonWayChallenge.WebService.Tests/XmlToJsonTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LemonWayChallenge.WebService.Tests
+{
+    [TestClass]
+    public class XmlToJsonTest
+    {
+        private Service m_service;
+        private TestContext m_testContext;
+
+        public Service ServiceContext
+        {
+            get { return m_service; }
+            set { m_service = value; }
+        }
+
+        public TestContext TestContext
+        {
+            get { return m_testContext; }
+            set { m_testContext = value; }
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ServiceContext = new Service();
+        }
+
+        [TestMethod]
+        [DeploymentItem("XmlToJsonTestData.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                           "|DataDirectory|\\XmlToJsonTestData.xml",
+                           "Row",
+                            DataAccessMethod.Random)]
+        public void XmlToJsonWorks()
+        {
+            string xml = TestContext.DataRow["XML"].ToString();
+            string expected = TestContext.DataRow["Result"].ToString();
+
+            string actual = ServiceContext.XmlToJson(xml);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            ServiceContext.Dispose();
+        }
+    }
+}
